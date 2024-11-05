517aef91109c871c34ba6c635a6dd8d0b0b3ab05
yusiheng <yusiheng@yusihengs-MacBook-Pro.local>
2024-11-05 23:32:07 +0800

    v2 = true

--- plugins/personmetalibx/__init__.py
+++ plugins/personmetalibx/__init__.py
@@ -38,7 +38,7 @@ class PersonMetaLibx(_PluginBase):
     # 插件图标
     plugin_icon = "https://raw.githubusercontent.com/yusiheng/MoviePilot-Plugins/main/icons/actorperson.png"
     # 插件版本
-    plugin_version = "2.1.0"
+    plugin_version = "2.1.1"
     # 插件作者
     plugin_author = "yusiheng"
     # 作者主页
